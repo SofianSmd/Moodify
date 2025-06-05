9bf984e2eca7977fbaf9beb87d924db64219351c
Sofian Smimid <sofian.smimid@etu.unilim.fr>
2025-06-06 01:40:46 +0200

    🎉 Initial commit - Moodify project setup

---
+++ Moodify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moodify
+//
+//  Created by Sofian Smimid on 06/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moodify/MoodifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodifyApp.swift
+//  Moodify
+//
+//  Created by Sofian Smimid on 06/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoodifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
